37f00d07b27776b6ae3dc002624a309bd1c16ee5
Anik Thomas <aniek.sw@gmail.com>
2022-01-12 14:50:59 -0700

    Formatting

--- src/components/FilterPage.js
+++ src/components/FilterPage.js
@@ -59,6 +59,4 @@ function FilterPage () {
     )
 }
 
-
-
 export default FilterPage;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -95,7 +95,7 @@ function Homepage () {
                             )        
                         })}
                     </Carousel>
-                </div>
+                </div>         
             ) 
         })}
         </div>
